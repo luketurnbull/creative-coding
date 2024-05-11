da87138ab17b75c80bf4b0e2d827479c45dc2cab
Luke David Turnbull <lukedturnbull@gmail.com>
2024-05-11 20:42:51 +1000

    Animate the lines to move like worms

--- flow-fields/script.js
+++ flow-fields/script.js
@@ -13,9 +13,41 @@ class Particle {
     this.effect = effect;
     this.x = Math.floor(Math.random() * this.effect.width);
     this.y = Math.floor(Math.random() * this.effect.height);
+    this.speedX = Math.random() * 2 - 1;
+    this.speedY = Math.random() * 2 - 1;
+    this.history = [{ x: this.x, y: this.y }];
+    this.maxLength = Math.floor(Math.random() * 150 + 10);
   }
+
   draw(context) {
-    context.fillRect(this.x, this.y, 40, 40);
+    context.fillRect(this.x, this.y, 10, 10);
+    context.beginPath();
+    context.moveTo(this.history[0].x, this.history[0].y);
+    for (let i = 1; i < this.history.length; i++) {
+      context.lineTo(this.history[i].x, this.history[i].y);
+    }
+    context.stroke();
+  }
+
+  update() {
+    if (this.x < 0) {
+      this.speedX = Math.abs(this.speedX);
+    } else if (this.x > this.effect.width) {
+      this.speedX = -Math.abs(this.speedX);
+    }
+
+    if (this.y < 0) {
+      this.speedY = Math.abs(this.speedY);
+    } else if (this.y > this.effect.height) {
+      this.speedY = -Math.abs(this.speedY);
+    }
+
+    this.x += this.speedX + Math.random() * 15 - 7.25;
+    this.y += this.speedY + Math.random() * 15 - 7.25;
+    this.history.push({ x: this.x, y: this.y });
+    if (this.history.length > this.maxLength) {
+      this.history.shift();
+    }
   }
 }
 
@@ -24,17 +56,47 @@ class Effect {
     this.width = width;
     this.height = height;
     this.particles = [];
+    this.numberOfParticles = 50;
+    //  this.cellSize = 20;
+    //  this.rows;
+    //  this.cols;
+    this.init();
   }
+
   init() {
-    this.particles.push(new Particle(this));
+    // flow field
+
+    // particles
+    for (let i = 0; i < this.numberOfParticles; i++) {
+      this.particles.push(new Particle(this));
+    }
   }
+
   render(context) {
     this.particles.forEach((particle) => {
+      particle.update(context);
       particle.draw(context);
     });
   }
 }
 
-const effect = new Effect(canvas.width, canvas.height);
-effect.init();
-effect.render(ctx);
+function init() {
+  const effect = new Effect(canvas.width, canvas.height);
+  effect.render(ctx);
+
+  const animate = () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    effect.render(ctx);
+    requestAnimationFrame(animate);
+  };
+
+  requestAnimationFrame(animate);
+}
+
+init();
+
+window.addEventListener("resize", function () {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  init();
+});
